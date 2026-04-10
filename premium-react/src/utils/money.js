9507b5d2ae9f373b3140cf21c4e4c5ff46d793a8
Nithin Gowda B K <nithinkempegowda2003@gmail.com>
2026-04-10 11:32:58 +0530

    Remove all repository contents

--- premium-react/src/utils/money.js
+++
@@ -1,8 +0,0 @@
-export function formatInr(value) {
-  const amount = Number(value || 0);
-  return new Intl.NumberFormat('en-IN', {
-    style: 'currency',
-    currency: 'INR',
-    maximumFractionDigits: 0,
-  }).format(amount);
-}
